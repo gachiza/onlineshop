8e9fd383ce13e14750f5bc0a006e7a2e36a5acd2
gachiza <gachiza17@gmail.com>
2024-03-15 11:30:48 +0300

    added commas

--- myecommerce/urls.py
+++ myecommerce/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns=[
-    path("", views.main, name="main")
-    path("cart/", views.cart, name="cart")
-    path("checkout/", views.checkout, name="checkout")
+    path("", views.main, name="main"),
+    path("cart/", views.cart, name="cart"),
+    path("checkout/", views.checkout, name="checkout"),
 ]
