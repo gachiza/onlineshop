e5f153f6966e9d9d50ff4c997c077a9404404437
gachiza <gachiza17@gmail.com>
2024-03-15 11:16:45 +0300

    created the funtions in views

--- myecommerce/views.py
+++ myecommerce/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return render(request, "ecommerce/index.html")
+def main(request):
+    return render(request, "ecommerce/main.html")
+
+def store(request):
+    return render(request, "ecommerce/store.html")
+
+def cart(request):
+    return render(request, "ecommerce/cart.html")
+
+def checkout(request):
+    return render(request, "ecommerce/checkout.html")
 
 # Create your views here.
