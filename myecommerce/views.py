172607926ca4d5e2ca9e53961325a1e304d672f7
gachiza <gachiza17@gmail.com>
2024-03-15 12:20:12 +0300

    updated views

--- myecommerce/views.py
+++ myecommerce/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def main(request):
-    return render(request, "ecommerce/main.html")
+
 
 def store(request):
-    return render(request, "ecommerce/store.html")
+    context={}
+    return render(request, "ecommerce/store.html", context)
 
 def cart(request):
-    return render(request, "ecommerce/cart.html")
+    context= {}
+    return render(request, "ecommerce/cart.html", context)
 
 def checkout(request):
-    return render(request, "ecommerce/checkout.html")
+    context= {}
+    return render(request, "ecommerce/checkout.html", context)
 
 # Create your views here.
