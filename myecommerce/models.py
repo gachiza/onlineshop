7d1cf30b95ecac5c2329ae5aa503cbbac67c086e
LIZ <elizabethnakiguli>
2024-03-18 16:09:38 +0300

    login form

--- myecommerce/models.py
+++ myecommerce/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class UserLogin(): 
+    class Meta:
+        model = User
+        fields = ['username', 'password']
 # Create your models here.
