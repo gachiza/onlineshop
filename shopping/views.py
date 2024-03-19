d69283b809f0550e1c0a99b4587faac203afcd00
Sarah Ssempala <sarahssempala0@gmail.com>
2024-03-19 07:59:39 +0000

    modified the views.py

--- shopping/views.py
+++ shopping/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Shopping
 
 
 def index (request):
     return render(request,'shopping/index.html')
 
 def shopping (request):
+    myshopping=Shopping.objects.all().values()
     template=loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    context = {
+    'myshopping': myshopping,
+  }
+    return HttpResponse(template.render(context, request))
 # Create your views here.
