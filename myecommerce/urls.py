a16f81e135a823fa800bca406b4749e6233045e7
gachiza <gachiza17@gmail.com>
2024-03-20 23:13:29 +0300

    updated urls

--- myecommerce/urls.py
+++ myecommerce/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
 from . import views 
 
-urlpatterns=[
-    path("", views.store, name="store"),
-    path("cart/", views.cart, name="cart"),
-    path('checkout/', views.checkout, name="checkout"),
-    path("login/", views.login, name="login"),
+urlpatterns = [
+        #Leave as empty string for base url
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+
 ]
