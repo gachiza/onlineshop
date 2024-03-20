1aa2f286bfa61f335879285c7432bdc9f340a467
gachiza <gachiza17@gmail.com>
2024-03-20 23:14:30 +0300

    updated views

--- myecommerce/views.py
+++ myecommerce/views.py
@@ -1,45 +1,14 @@
-from django.shortcuts import render, HttpResponse
-from .models import *
-from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
-from .forms import *
-# Create your views here.
-
-
-def registration(request):
-    context={}
-    return render(request, "ecommerce/registration.html", context)
-
-def footer(request):
-    context={}
-    return render(request, "ecommerce/footer.html", context)
+from django.shortcuts import render
 
 
 def store(request):
-    context={}
-    return render(request, "ecommerce/store.html", context)
+     context = {}
+     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context= {}
-    return render(request, "ecommerce/cart.html", context)
+     context = {}
+     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context= {}
-    return render(request, "ecommerce/checkout.html", context)
-
-# Create your views here.
-def login(request):
-    if request.method == 'POST':
-        form = UserLogin(request.POST)
-        if form.is_valid():
-            username = request.POST.get("username")
-            password = request.POST.get("password")
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                return HttpResponse('logged in')
-    else:
-        form = UserLogin()
-        context = {
-            "form": form
-        }
-    return render(request, "ecommerce/login.html",context)
+      context = {}
+      return render(request, 'store/checkout.html', context)
