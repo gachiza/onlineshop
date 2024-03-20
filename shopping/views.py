85aaf79910e94841aff4c7a11e350119afb1426a
Sarah Ssempala <sarahssempala0@gmail.com>
2024-03-20 16:46:52 +0000

    added some static files

--- shopping/views.py
+++ shopping/views.py
@@ -30,8 +30,9 @@ def main(request):
   return HttpResponse(template.render())
 
 def testing(request):
-  template = loader.get_template('template.html')
+  mydata = Shopping.objects.all()
+  template = loader.get_template('temp.html')
   context = {
-    'fruits': ['Apple', 'Banana', 'Cherry'],   
+    'myshopping': mydata,
   }
-  return render(request, 'testing.html')
+  return HttpResponse(template.render(context, request))
