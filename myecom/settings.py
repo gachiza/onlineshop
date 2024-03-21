40f3f1ffc03093542696dec1013f553557666f6c
gachiza <gachiza17@gmail.com>
2024-03-21 11:42:56 +0300

    updated settings

--- myecom/settings.py
+++ myecom/settings.py
@@ -126,5 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR,"static/images")
