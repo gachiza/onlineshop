d9ae80497ab20967f1a0182f7e416195f45c2aca
gachiza <gachiza17@gmail.com>
2024-03-21 11:44:53 +0300

    updated models

--- myecommerce/models.py
+++ myecommerce/models.py
@@ -13,10 +13,18 @@ class Product(models.Model):
     name= models.CharField(max_length=200)
     price= models.FloatField()
     digital = models.BooleanField(default=False,null=True,blank=True)
-    #image
+    image= models.ImageField(null=True, blank= True)
 
     def __str__(self):
         return self.name
+    
+    @property
+    def imageURL(self):
+        try:
+            url=self.image.url
+        except:
+            url=''
+        return url
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
@@ -26,6 +34,18 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+    
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total= sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems= self.orderitem_set.all()
+        total= sum([item.quantity for item in orderitems])
+        return total 
 
 
 class OrderItem(models.Model):
@@ -34,12 +54,18 @@ class OrderItem(models.Model):
     quantity= models.IntegerField(default=0, null=True, blank=True)
     date_added= models.DateTimeField(auto_now_add=True)
 
+    @property
+    def get_total(self):
+        total= self.product.price * self.quantity
+        return total
+
 
 class DeliveryAddress(models.Model):
     customer= models.ForeignKey(Customer, on_delete=models.SET_NULL, null= True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     adress= models.CharField(max_length=200,null=False)
     city= models.CharField(max_length=200,null=False)
+    building = models.CharField(max_length=200,null=True)
     date_added= models.DateTimeField(auto_now_add=True)
 
 
