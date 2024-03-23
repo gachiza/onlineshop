47db81c96ee01d4cfea6097bb90a4e8bd805c91e
gachiza <gachiza17@gmail.com>
2024-03-24 01:44:24 +0300

    updated urls

--- myecommerce/urls.py
+++ myecommerce/urls.py
@@ -4,7 +4,13 @@ from . import views
 urlpatterns = [
         #Leave as empty string for base url
 	path('', views.store, name="store"),
+    path('home/', views.home, name= "home"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
+    path('login/', views.login, name="login"),
+    path('signup/', views.signup, name= "signup"),
+    path('update_item/', views.updateItem, name="update_-item"),
+    path('items/', views.item_list, name='item_list'),
+    
 
 ]
