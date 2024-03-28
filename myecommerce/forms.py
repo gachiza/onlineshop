6d48abfced804150a03061609011c3434e5838d8
ssekkanzi elvis <abdulssekyanzi@gmail.com>
2024-03-28 19:41:45 +0300

    i created a groupp form in forms.py

--- myecommerce/forms.py
+++ myecommerce/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django import forms
 from .models import Customer
+from .models import Group
 
 class UserLogin(): 
     class Meta:
@@ -10,3 +11,7 @@ class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ['first_name', 'last_name', 'image']
+class GroupForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = ['name', 'image']
